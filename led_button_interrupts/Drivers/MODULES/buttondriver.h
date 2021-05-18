3024589959ec2638b6f7ccf9b1f8814a4e717bd3
MUHAMMED ERKAM OKUMUŞ <l1418052@std.yildiz.edu.tr>
2021-05-18 17:48:05 +0300

    STM32F4

---
+++ led_button_interrupts/Drivers/MODULES/buttondriver.h
@@ -0,0 +1,17 @@
+/*
+ * buttondriver.h
+ *
+ *  Created on: Mar 18, 2021
+ *      Author: Numan
+ */
+
+#ifndef MODULS_BUTTONDRIVER_H_
+#define MODULS_BUTTONDRIVER_H_
+
+void buttondriver_init();
+void buttonDriverInt_Init();
+int buttondriver_get_state();
+
+
+
+#endif /* MODULS_BUTTONDRIVER_H_ */
